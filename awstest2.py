528ab191c68ff6f4efb203c5773b58a5b98e9c13
dap-i <45938848+dap-i@users.noreply.github.com>
2023-06-23 23:00:13 +0200

    Update awstest2.py

--- awstest2.py
+++ awstest2.py
@@ -206,7 +206,7 @@ elif app_mode == 'alle Wurfe':
     x = np.random.normal(5, size=5000)
     y = np.random.normal(4, size=5000)
 
-    fig, ax = plt.subplots(figsize=(6,5))
+    fig, ax = plt.subplots(figsize=(6,4))
     plt.hist2d(x,y, bins=[np.arange(0,6,1),np.arange(0,5,1)], alpha=0.4)
     plt.colorbar()
     ax.imshow(background_array, extent=[0, 6, 0, 4], aspect='auto')
