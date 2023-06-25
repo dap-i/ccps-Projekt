f42a8df744a6e9db896ef4dc6e1f647499e78ac3
dap-i <45938848+dap-i@users.noreply.github.com>
2023-06-25 23:57:09 +0200

    Update Visualisierung.py

--- Visualisierung.py
+++ Visualisierung.py
@@ -96,7 +96,7 @@ if app_mode == 'Einzelwurf':
         for j in range(5):
             ax[i][j].imshow(blocks[j][i])
             ax[i][j].axis('off')
-    data_coords = [(X_P, Y_P)]
+    data_coords = [(2, 2)]
     for coord in data_coords:
         row, col = coord
     if (row, col) == data_coords[0]:
